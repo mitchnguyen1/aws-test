3720fa66b88ff1cf4079a6d548bafcc11b133831
Mitchell Nguyen <mitch01nguyen@gmail.com>
2023-05-15 11:55:36 -0500

    changes url

--- public/index.js
+++ public/index.js
@@ -12,7 +12,7 @@ const lossesTest = document.querySelector("#losses");
 const chooseHeader = document.querySelector("#choose-header");
 const yourDuoHeader = document.querySelector("#your-duo-header");
 const compDuoHeader = document.querySelector("#comp-duo-header");
-const baseURL = `http://localhost:8000`
+const baseURL = `http://52.24.147.115/`
 
 let choices = [];
 let compDuo = [];
