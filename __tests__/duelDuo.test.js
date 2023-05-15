ecfee89b65d2a0a4d1b9c66796a33090c19056cc
Lucky Phan <lpt.lucky@gmail.com>
2023-05-15 07:09:39 -0400

    debugged

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -12,24 +12,24 @@ afterEach(async () => {
 
 describe("Duel Duo tests", () => {
   test("page loads with title", async () => {
-    await driver.get("http://localhost:3000");
+    await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
   test("press draw displays choices", async () => {
-    await driver.get("http://localhost:3000");
+    await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
     await driver.findElement(By.id("draw")).click()
     await driver.findElement(By.id("choices"))
   });
   test("press add to duo displays player-duo", async () => {
-    await driver.get("http://localhost:3000");
+    await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
     await driver.findElement(By.id("draw")).click()
     await driver.findElement(By.className("bot-btn")).click()
     await driver.findElement(By.id("player-duo"))
   });
   // test("press removed from duo shows that element return to choices", async () => {
-  //   await driver.get("http://localhost:3000");
+  //   await driver.get("http://localhost:8000");
   //   await driver.wait(until.titleIs("Duel Duo"), 1000);
   //   await driver.findElement(By.id("draw")).click()
   //   const robotToRemove = await driver.findElement(By.className("bot-btn"))
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+//need to npm install dotenv
+require('dotenv').config()
+const path = require('path')
 const express = require("express");
 const bots = require("./src/botsData");
 const shuffle = require("./src/shuffle");
@@ -8,8 +11,21 @@ const playerRecord = {
 };
 const app = express();
 
+const {ROLLBAR_ACCESS_TOKEN} = process.env
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 // Add up the total health of all the robots
 const calculateTotalHealth = (robots) =>
@@ -38,8 +54,10 @@ const calculateHealthAfterAttack = ({ playerDuo, compDuo }) => {
 
 app.get("/api/robots", (req, res) => {
   try {
-    res.status(200).send(botsArr);
+    rollbar.info('someone requested the list of robots')
+    res.status(200).send(bots);
   } catch (error) {
+    rollbar.critical(`get list of robots failed`)
     console.error("ERROR GETTING BOTS", error);
     res.sendStatus(400);
   }
@@ -47,9 +65,11 @@ app.get("/api/robots", (req, res) => {
 
 app.get("/api/robots/shuffled", (req, res) => {
   try {
+    rollbar.info('someone requested list of shuffled robots')
     let shuffled = shuffle(bots);
     res.status(200).send(shuffled);
   } catch (error) {
+    rollbar.warning('error getting shuffled bots')
     console.error("ERROR GETTING SHUFFLED BOTS", error);
     res.sendStatus(400);
   }
@@ -57,6 +77,7 @@ app.get("/api/robots/shuffled", (req, res) => {
 
 app.post("/api/duel", (req, res) => {
   try {
+    rollbar.info('someone requested pairs of robot duel')
     const { compDuo, playerDuo } = req.body;
 
     const { compHealth, playerHealth } = calculateHealthAfterAttack({
@@ -67,9 +88,11 @@ app.post("/api/duel", (req, res) => {
     // comparing the total health to determine a winner
     if (compHealth > playerHealth) {
       playerRecord.losses += 1;
+      rollbar.info(`User has lost ${playerRecord.losses} times`)
       res.status(200).send("You lost!");
     } else {
       playerRecord.wins += 1;
+      rollbar.info(`User has won ${playerRecord.wins} times`)
       res.status(200).send("You won!");
     }
   } catch (error) {
